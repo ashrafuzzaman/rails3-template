7fee90870c6b432ef3a64920bff51ecc1c95556d
A.K.M. Ashrafuzzaman <jitu@dragon.(none)>
2012-05-11 16:53:58 +0600

    generator working with view files

--- lib/generators/nested_scaffold/nested_scaffold_generator.rb
+++ lib/generators/nested_scaffold/nested_scaffold_generator.rb
@@ -2,14 +2,17 @@ require_relative 'resource'
 
 class NestedScaffoldGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
+  argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
+
   attr_accessor :pr, :r
 
   def copy_view_files
   	r_name, pr_name = file_name.split('/')
     self.r, self.pr = Resource.new(r_name), Resource.new(pr_name)
+
     available_views.each do |view|
-    	file = "#{view}.html.erb"
-      template "erb/scaffold/#{file}", "app/views/#{r.plural_table_name}/#{file}"
+      filename = "#{view}.html.erb"
+      template "erb/#{filename}", File.join("app/views", r.plural_table_name, filename)
     end
   end
 
