5dc5748b100bf787bf61ae00fca1ac09b5ad977a
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2012-06-08 18:20:10 +0600

    Added new rails nested scaffold generator

--- lib/generators/nested_scaffold/nested_scaffold_generator.rb
+++ lib/generators/nested_scaffold/nested_scaffold_generator.rb
@@ -1,98 +1,42 @@
+require 'rails/generators/rails/scaffold/scaffold_generator'
 require_relative 'resource'
-require 'rails/generators/resource_helpers'
 
-class NestedScaffoldGenerator < Rails::Generators::NamedBase
+class NestedScaffoldGenerator < Rails::Generators::ScaffoldGenerator
   source_root File.expand_path('../templates', __FILE__)
-  argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
-  class_option :stylesheets, :type => :boolean, :desc => "Generate Stylesheets"
-  class_option :stylesheet_engine, :desc => "Engine for Stylesheets"
 
-  def initialize(args, *options) #:nodoc:
-    parse_reources!(args[0])
-    args[0] = r.name
-    args << "#{r.name.downcase}:references"
-		super(args, *options)
+  def create_controller_files
+    template "controller.rb", File.join('app/controllers', class_path, "#{controller_file_name}_controller.rb")
   end
 
   def copy_view_files
     available_views.each do |view|
       filename = "#{view}.html.erb"
-      template "erb/#{filename}", File.join("app/views", r.plural_table_name, filename)
+      template "erb/#{filename}", File.join("app/views", plural_table_name, filename)
     end
   end
 
-  def copy_controller
-    template 'controller.rb', File.join('app/controllers', class_path, "#{r.name}_controller.rb")
-  end
+  protected
 
-  #def add_routes
-  #  actions.reverse.each do |action|
-  #    route %{get "#{file_name}/#{action}"}
-  #  end
-  #end
+	def singular_path
+		"#{pr.singular_table_name}_#{singular_table_name}_path"
+	end
 
-  hook_for :test_framework, :in => :rails, :as => :scaffold
-  hook_for :messages
-  hook_for :helper, :in => :rails, :as => :scaffold do |invoked|
-    invoke invoked, [ controller_name ]
-  end
-
-  hook_for :orm, :required => true
-
-	protected
-
-  def available_views
+	def available_views
     %w(index show new edit _form)
   end
 
-	def parse_reources!(name)
-  	pr_name, r_name = name.split(':')
-    @r  ||= Resource.new(r_name.downcase)
-    @pr ||= Resource.new(pr_name.downcase)
-	end
-
-	def r
-		@r
-	end
+  def parse_attributes!
+  	parent_ar = self.attributes.to_s.scan(/(\w*):references/)[0]
+  	if parent_ar
+  		parent = parent_ar[0]
+  		say(parent, :green)
+  		@pr ||= Resource.new(parent.downcase)
+  	end
+    super
+  end
 
 	def pr
-		@pr
+    @pr
 	end
 
-	def controller_name
-		"#{class_name}_controller"
-	end
-
-	def index_helper_path
-		uncountable? ? "#{pr_singular_r_plural}_index_path(@#{pr.singular_table_name}})" : "#{pr_singular_r_plural}_path(@#{pr.singular_table_name})"
-	end
-
-	def edit_link_path
-		"edit_#{singular_path}"
-	end
-
-	def singular_path
-		"#{pr_singular_r_singular}_path"
-	end
-
-	def pr_singular_r_singular
-		"#{pr.singular_table_name}_#{r.singular_table_name}"
-	end
-
-	def pr_singular_r_plural
-		"#{pr.singular_table_name}_#{r.plural_table_name}"
-	end
-
-	def controller_class_name
-		"#{r.class_name}Controller"
-	end
-
-end
-
-unless Kernel.respond_to?(:require_relative)
-  module Kernel
-    def require_relative(path)
-      require File.join(File.dirname(caller[0]), path.to_str)
-    end
-  end
 end
--- lib/generators/nested_scaffold/templates/controller.rb
+++ lib/generators/nested_scaffold/templates/controller.rb
@@ -4,68 +4,68 @@ class <%= controller_class_name %>Controller < ApplicationController
   before_filter :load_<%= pr.singular_table_name %>
 
   def index
-  	@<%= r.plural_table_name %> = @<%= pr.singular_table_name %>.<%= r.plural_table_name %>
-    respond_with(@<%= r.plural_table_name %>)
+  	@<%= plural_table_name %> = @<%= pr.singular_table_name %>.<%= plural_table_name %>
+    respond_with(@<%= plural_table_name %>)
   end
 
   def show
-    @<%= r.orm_instance %> = @<%= pr.singular_table_name %>.<%= r.plural_table_name %>.find params[:id]
-    respond_with(@<%= r.orm_instance %>)
+    @<%= singular_table_name %> = @<%= pr.singular_table_name %>.<%= plural_table_name %>.find params[:id]
+    respond_with(@<%= singular_table_name %>)
   end
 
   def new
-    @<%= r.orm_instance %> = @<%= pr.orm_instance %>.<%= r.plural_table_name %>.new
+    @<%= singular_table_name %> = @<%= pr.singular_table_name %>.<%= plural_table_name %>.new
   end
 
   def edit
-    @<%= r.orm_instance %> = @<%= pr.orm_instance %>.<%= r.plural_table_name %>.find params[:id]
+    @<%= singular_table_name %> = @<%= pr.singular_table_name %>.<%= plural_table_name %>.find params[:id]
   end
 
   def create
-  	@<%= r.orm_instance %> = @<%= pr.orm_instance %>.<%= r.plural_table_name %>.build params[:<%= r.orm_instance%>]
+  	@<%= singular_table_name %> = @<%= pr.singular_table_name %>.<%= plural_table_name %>.build params[:<%= singular_table_name%>]
 
     respond_to do |format|
-      if @<%= r.orm_instance %>.save
-        format.html { redirect_to @<%= r.orm_instance %>, notice: <%= "'#{r.human_name} was successfully created.'" %> }
-        format.json { render json: <%= "@#{r.orm_instance}" %>, status: :created }
-        format.js { flash[:notice] = <%= "'#{r.human_name} was created.'" %> }
+      if @<%= singular_table_name %>.save
+        format.html { redirect_to @<%= singular_table_name %>, notice: <%= "'#{human_name} was successfully created.'" %> }
+        format.json { render json: <%= "@#{singular_table_name}" %>, status: :created }
+        format.js { flash[:notice] = <%= "'#{human_name} was created.'" %> }
       else
         format.html { render action: "new" }
-        format.json { render json: <%= "@#{r.orm_instance}.errors" %>, status: :unprocessable_entity }
-        format.js { flash[:error] = @<%= r.orm_instance %>.errors }
+        format.json { render json: <%= "@#{singular_table_name}.errors" %>, status: :unprocessable_entity }
+        format.js { flash[:error] = @<%= singular_table_name %>.errors }
       end
     end
   end
 
   def update
-    @<%= r.orm_instance %> = @<%= pr.orm_instance %>.<%= r.plural_table_name %>.find params[:id]
+    @<%= singular_table_name %> = @<%= pr.singular_table_name %>.<%= plural_table_name %>.find params[:id]
 
     respond_to do |format|
-      if @<%= r.orm_instance %>.update_attributes(params[:<%= r.singular_table_name %>])
-        format.html { redirect_to @<%= r.orm_instance %>, notice: <%= "'#{r.human_name} was successfully updated.'" %> }
+      if @<%= singular_table_name %>.update_attributes(params[:<%= singular_table_name %>])
+        format.html { redirect_to @<%= singular_table_name %>, notice: <%= "'#{human_name} was successfully updated.'" %> }
         format.json { head :no_content }
-        format.js { flash[:notice] = <%= "'#{r.human_name} was successfully updated.'" %> }
+        format.js { flash[:notice] = <%= "'#{human_name} was successfully updated.'" %> }
       else
         format.html { render action: "edit" }
-        format.json { render json: <%= "@#{r.orm_instance}.errors" %>, status: :unprocessable_entity }
-        format.js { flash[:error] = @<%= r.orm_instance %>.errors }
+        format.json { render json: <%= "@#{singular_table_name}.errors" %>, status: :unprocessable_entity }
+        format.js { flash[:error] = @<%= singular_table_name %>.errors }
       end
     end
   end
 
   def destroy
-    @<%= r.orm_instance %> = @<%= pr.orm_instance %>.<%= r.plural_table_name %>.find params[:id]
-    @<%= r.orm_instance %>.destroy
+    @<%= singular_table_name %> = @<%= pr.singular_table_name %>.<%= plural_table_name %>.find params[:id]
+    @<%= singular_table_name %>.destroy
 
     respond_to do |format|
-      format.html { redirect_to <%= index_helper_path %> }
+      format.html { redirect_to <%= index_helper %>_url }
       format.json { head :no_content }
     end
   end
 
   private
   def load_<%= pr.singular_table_name %>
-  	@<%= pr.orm_instance %> = <%= pr.class_name %>.find params[:<%= pr.singular_table_name %>_id]
+  	@<%= pr.singular_table_name %> = <%= pr.class_name %>.find params[:<%= pr.singular_table_name %>_id]
   end
 end
 <% end -%>
