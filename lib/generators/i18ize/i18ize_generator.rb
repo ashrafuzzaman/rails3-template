4090cb6beb3050d3f337e290140a3b3828ac8445
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2012-06-19 18:05:36 +0600

    Replaced text in the file

--- lib/generators/i18ize/i18ize_generator.rb
+++ lib/generators/i18ize/i18ize_generator.rb
@@ -3,14 +3,14 @@ class I18izeGenerator < Rails::Generators::NamedBase
   
   def replace
     files.each do |file|
-      gsub_file file, /rake/, :green do |match|
-        match << " no more. Use thor!"
+      gsub_file file, /rake/i do |match|
+        "RAKE no more. Use thor!"
       end 
     end
   end
 
   private
   def files
-    %w(app/controllers/#{controller_file_name}_controller.rb)
+    ["app/controllers/#{plural_table_name}_controller.rb"]
   end
 end
