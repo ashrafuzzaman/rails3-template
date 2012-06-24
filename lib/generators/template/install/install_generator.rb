e0f1a0578a70d53e721c7a91d45b1fd88fde05e2
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2012-06-24 16:04:02 +0600

    Fixed message templates

--- lib/generators/template/install/install_generator.rb
+++ lib/generators/template/install/install_generator.rb
@@ -2,35 +2,37 @@ require 'rails/generators'
 
 module Template
   module Generators
-		class InstallGenerator < Rails::Generators::NamedBase
-		  source_root File.expand_path('../templates', __FILE__)
-
-		  def copy_view_templates
-		    available_view_templates.each do |file|
-		      copy_file "erb/scaffold/#{name}/#{file}.html.erb", 
+    class InstallGenerator < Rails::Generators::NamedBase
+      source_root File.expand_path('../templates', __FILE__)
+      def copy_view_templates
+        available_view_templates.each do |file|
+          copy_file "erb/scaffold/#{name}/#{file}.html.erb",
 		      			"lib/templates/erb/scaffold/#{file}.html.erb"
-		    end
-		  end
-
-		  def copy_controller_template
-		  	file_with_ext = "rails/scaffold_controller/controller.rb"
-	        copy_file file_with_ext, "lib/templates/#{file_with_ext}"
-		  end
-
-		  def copy_helper_template
-		  	file_with_ext = "rails/helper/helper.rb"
-	        copy_file file_with_ext, "lib/templates/#{file_with_ext}"
-		  end
-
-      def copy_shared_templates
-        file_with_ext = "erb/scaffold/#{name}/shared/_notifications.html.erb"
-          copy_file file_with_ext, "app/views/shared/_notifications.html.erb"
+        end
       end
 
-		  private
-		  	def available_view_templates
-		      %w(show new edit _form index)
-	    	end
-		end
-	end
+      def copy_controller_template
+        file_with_ext = "rails/scaffold_controller/controller.rb"
+        copy_file file_with_ext, "lib/templates/#{file_with_ext}"
+      end
+
+      def copy_helper_template
+        file_with_ext = "rails/helper/helper.rb"
+        copy_file file_with_ext, "lib/templates/#{file_with_ext}"
+      end
+
+      def copy_shared_templates
+        ['message', 'error'].each do |file|
+          file_with_ext = "erb/scaffold/#{name}/shared/_#{file}.html.erb"
+          copy_file file_with_ext, "app/views/shared/_#{file}.html.erb"
+        end
+      end
+
+      private
+
+      def available_view_templates
+        %w(show new edit _form index)
+      end
+    end
+  end
 end
